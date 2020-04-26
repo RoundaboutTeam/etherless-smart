370a25293328237830c410946370190a05cee874
Veronica Barbieri <veronica.barbieri.p@gmail.com>
2020-04-26 21:05:02 +0200

    Added getBalance and getData functions and corrected developer adress in addFunction
    - Added "getBalance" and "getData" functions
    - Corrected "addFunction": now the developer's adress is taken from "msg.sender" instead of the function parameter "developer"
    
    (updated EtherlessSmart.json accordingly)

--- Contracts/EtherlessSmart.sol
+++ Contracts/EtherlessSmart.sol
@@ -24,48 +24,57 @@ contract EtherlessSmart {
     event response(string _result, string id);
 
     // functions to check list (availableFunctions)
-        //getList -> returns the full list of available functions
-        function getList() public view returns (string[] memory) {
-            return functionNames;
-        }
+    //getList -> returns the full list of available functions
+    function getList() public view returns (string[] memory) {
+      return functionNames;
+    }
 
-        //existsFunction -> check if function is in the availableFunctions list
-        function existsFunction(string memory name) public view returns (bool) {
-            if(availableFunctions[name].exists) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+    //existsFunction -> check if function is in the availableFunctions list
+    function existsFunction(string memory name) public view returns (bool) {
+      if(availableFunctions[name].exists) {
+          return true;
+      } else {
+          return false;
+      }
+    }
 
-        //addFunction -> add deployed function to list
-        function addFunction(string memory name, uint256 price, address payable developer) public {
-            availableFunctions[name] = jsFunction(name, price, developer, true);
-            functionNames.push(name); //check if length is correct after adding an element
-        }
+    //addFunction -> add deployed function to list
+    function addFunction(string memory name, uint256 price) public {
+      address payable developer = msg.sender;
+      availableFunctions[name] = jsFunction(name, price, developer, true);
+      functionNames.push(name); //check if length is correct after adding an element
+    }
 
-        //removeFunction -> remove function from availableFunctions list
-        function removeFunction(string memory toRemove) public {
-            delete availableFunctions[toRemove];
-            for (uint index = 0; index < functionNames.length; index++) {
-                if(uint(keccak256(abi.encodePacked(functionNames[index]))) == uint(keccak256(abi.encodePacked(toRemove)))){
-                    delete functionNames[index]; //check if length is correct after deleting an element
-                }
-            }
-        }
+    //removeFunction -> remove function from availableFunctions list
+    function removeFunction(string memory toRemove) public {
+      delete availableFunctions[toRemove];
+      for (uint index = 0; index < functionNames.length; index++) {
+          if(uint(keccak256(abi.encodePacked(functionNames[index]))) == uint(keccak256(abi.encodePacked(toRemove)))){
+              delete functionNames[index]; //check if length is correct after deleting an element
+          }
+      }
+    }
 
-    // main commands
-        //runFunction -> requests execution of the function (call to existsFunction - requests payment - check payment - emit executeFunction event)
-        function runFunction(string memory funcName, string memory param, string memory id) public payable {
-            data = funcName;
-            balance += msg.value;
-            emit runRequest(funcName, param, id);
-        }
+// main commands
+    //runFunction -> requests execution of the function (call to existsFunction - requests payment - check payment - emit executeFunction event)
+    function runFunction(string memory funcName, string memory param, string memory id) public payable {
+      data = funcName;
+      balance += msg.value;
+      emit runRequest(funcName, param, id);
+    }
 
-        function resultFunction(string memory result, string memory id) public {
-            emit response(result, id);
-        }
-        //deployFunction -> deployes the user developed function
-        //deleteFunction -> delete one of the caller's owned functions
-        //...
+    function resultFunction(string memory result, string memory id) public {
+      emit response(result, id);
+    }
+    //deployFunction -> deployes the user developed function
+    //deleteFunction -> delete one of the caller's owned functions
+    //...
+
+    function getBalance() public view returns (uint256){
+      return balance;
+   }
+
+   function getData() public view returns(string memory){
+     return data;
+   }
 }
