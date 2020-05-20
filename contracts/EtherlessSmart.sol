c07ef43fa7eb39ea76c95f54ffabd684ff320128
Veronica Barbieri <veronica.barbieri.p@gmail.com>
2020-05-20 12:18:09 +0200

    Aggiunte dipendenze Openzeppelin e separati contratti in Smart e Storage

--- contracts/EtherlessSmart.sol
+++ contracts/EtherlessSmart.sol
@@ -1,25 +1,15 @@
 pragma solidity >=0.4.22 <0.7.0;
 
-//TO DO//
-//getFuncInfo -> returns all the info stored in the mapping
-//deployFunction -> deployes the user developed function
-//deleteFunction -> delete one of the caller's owned functions in AWS
+import './EtherlessStorage.sol';
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
 
-contract EtherlessSmart {
+contract EtherlessSmart is Initializable {
+
   address ownerAddress;
-  uint256 balance = 0;
-  uint256 requestId; //incremented at every run request
+  uint256 contractBalance;
+  uint256 requestId;
 
-  struct jsFunction {
-    string name;
-    //maybe add function signature!
-    uint256 price; // wei
-    address payable developer;
-    bool exists;
-  }
-
-  mapping (string => jsFunction) private availableFunctions; //check if struct in mapping is set to 0 by default
-  bytes32[] functionNames;
+  EtherlessStorage private functions;
 
   //events
   //event run
@@ -27,76 +17,23 @@ contract EtherlessSmart {
   //event response
   event response(string result, uint256 indexed id);
 
-  constructor() public {
-    ownerAddress = msg.sender;
-    requestId = 0;
-  }
-
   modifier onlyOwner(address _invokedFrom) {
     require(_invokedFrom == ownerAddress, "You are not the owner of the contract!");
     _;
   }
 
-  //FUNCTIONS THAT ENABLE TYPE CONVERSION BETWEEN STRING AND BYTES32
-  //converts a string to bytes32
-  function stringToBytes(string memory source) public pure returns (bytes32 result) {
-    bytes memory tempEmptyStringTest = bytes(source);
-    if(tempEmptyStringTest.length == 0) {
-        return 0x0;
-    } assembly {
-        result := mload(add(source, 32))
-    }
+  function initialize () public initializer {
+    contractBalance = 0;
+    requestId = 0;
+    ownerAddress = msg.sender;
   }
 
-  //converts a bytes32 to string
-  function bytes32ToStr(bytes32 _bytes32) public pure returns (string memory) {
-    bytes memory bytesArray = new bytes(32);
-    for (uint256 i; i < 32; i++) {
-        bytesArray[i] = _bytes32[i];
-        }
-    return string(bytesArray);
-    }
-
-  //FUNCTIONS THAT IMPLEMENT OPERATIONS ON THE AVAILABLEFUNCTIONS LIST
-  //getList -> returns the full list of names of available functions
-  function getList() public view returns (bytes32[] memory) {
-    return functionNames;
-  }
-
-  //existsFunction -> checks if a certain function is in the availableFunctions list
-  function existsFunction(string memory name) public view returns (bool) {
-    if(availableFunctions[name].exists) {
-        return true;
-    } else {
-        return false;
-    }
-  }
-
-  //addFunction -> adds a function that has just been deployed to the list
-  function addFunction(string memory name, uint256 price) public {
-    address payable developer = msg.sender;
-    availableFunctions[name] = jsFunction(name, price, developer, true);
-    functionNames.push(stringToBytes(name));
-  }
-
-  //removeFunction -> removes a given function from availableFunctions list
-  function removeFunction(string memory toRemove) public {
-    delete availableFunctions[toRemove];
-    for (uint index = 0; index < functionNames.length; index++) {
-        if(functionNames[index] == stringToBytes(toRemove)){
-            //delete functionNames[index]; //check if length is correct after deleting an element
-            functionNames[index] = functionNames[functionNames.length - 1];
-            functionNames.pop();
-        }
-    }
-  }
-
-//MAIN COMMANDS
+  //MAIN COMMANDS
   //runFunction -> requests execution of the function
   function runFunction(string memory funcName, string memory param) public payable {
-    require(existsFunction(funcName), "The function you're looking for does not exist! :'(");
-    uint256 funcPrice = availableFunctions[funcName].price;
-    address payable funcDev = availableFunctions[funcName].developer;
+    require(functions.existsFunction(funcName), "The function you're looking for does not exist! :'(");
+    uint256 funcPrice = functions.availableFunctions[funcName].price;
+    address payable funcDev = functions.availableFunctions[funcName].developer;
 
     require(msg.value >= funcPrice, "Insufficient amount sent! :'(");
     balance += msg.value;
@@ -126,4 +63,73 @@ contract EtherlessSmart {
  function getNewId() private returns (uint256){
    return requestId++;
  }
+
 }
+/*pragma solidity >=0.4.22 <0.7.0;
+
+import './EtherlessStorage.sol';
+
+//TO DO//
+//getFuncInfo -> returns all the info stored in the mapping
+//deployFunction -> deployes the user developed function
+//deleteFunction -> delete one of the caller's owned functions in AWS
+
+contract EtherlessSmart {
+  address ownerAddress;
+  uint256 balance = 0;
+  uint256 requestId; //incremented at every run request
+
+  EtherlessStorage private functions;
+
+  //events
+  //event run
+  event runRequest(string funcname, string param, uint256 indexed id);
+  //event response
+  event response(string result, uint256 indexed id);
+
+  constructor() public {
+    ownerAddress = msg.sender;
+    requestId = 0;
+  }
+
+  modifier onlyOwner(address _invokedFrom) {
+    require(_invokedFrom == ownerAddress, "You are not the owner of the contract!");
+    _;
+  }
+
+//MAIN COMMANDS
+  //runFunction -> requests execution of the function
+  function runFunction(string memory funcName, string memory param) public payable {
+    require(functions.existsFunction(funcName), "The function you're looking for does not exist! :'(");
+    uint256 funcPrice = functions.availableFunctions[funcName].price;
+    address payable funcDev = functions.availableFunctions[funcName].developer;
+
+    require(msg.value >= funcPrice, "Insufficient amount sent! :'(");
+    balance += msg.value;
+
+    sendAmount(funcDev, funcPrice);
+
+    getNewId();
+    emit runRequest(funcName, param, requestId);
+  }
+
+  //resultFunction -> returns the result of a function execution
+  function resultFunction(string memory result, uint256 id) public onlyOwner(msg.sender){
+    emit response(result, id);
+  }
+
+  function getBalance() public view returns (uint256){
+    return balance;
+ }
+
+  //sendAmount -> sends the given amount to a certain address
+ function sendAmount(address payable to, uint256 amount) public {
+   balance -= amount; //remainder from payment
+   to.transfer(amount);
+ }
+
+  //getNewId -> increments requestId
+ function getNewId() private returns (uint256){
+   return requestId++;
+ }
+}*/
---
+++ contracts/EtherlessStorage.sol
@@ -0,0 +1,113 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract EtherlessStorage {
+
+struct jsFunction {
+    string name;
+    //maybe add function signature!
+    uint256 price; // wei
+    address payable developer;
+    bool exists;
+  }
+
+  mapping (string => jsFunction) private availableFunctions; //check if struct in mapping is set to 0 by default
+  string[] functionNames;
+
+  //FUNCTIONS THAT IMPLEMENT OPERATIONS ON THE AVAILABLEFUNCTIONS LIST
+  //existsFunction -> checks if a certain function is in the availableFunctions list
+  function existsFunction(string memory name) public view returns (bool) {
+    if(availableFunctions[name].exists) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+  //addFunction -> adds a function that has just been deployed to the list
+function addFunction(string memory name, uint256 price) public {
+    address payable developer = msg.sender;
+    availableFunctions[name] = jsFunction(name, price, developer, true);
+    functionNames.push(name);
+}
+
+//removeFunction -> removes a given function from availableFunctions list
+    function removeFunction(string memory toRemove) public {
+        delete availableFunctions[toRemove];
+        for (uint index = 0; index < functionNames.length; index++) {
+            if(keccak256(abi.encodePacked(functionNames[index])) == keccak256(abi.encodePacked(toRemove))){
+                //delete functionNames[index]; //check if length is correct after deleting an element
+                functionNames[index] = functionNames[functionNames.length - 1];
+                functionNames.pop();
+            }
+        }
+    }
+
+    //returns the function price
+    function getFuncPrice(string memory _funcName) public view returns(uint256){
+        uint256 _price = availableFunctions[_funcName].price;
+        return _price;
+    }
+
+    //returns the list of functions in this format (all in one line) :
+    /*
+    {
+        [
+        {"name":"function name","price":"price of function","developer","developer address"},
+        {"name":"function name 2","price":"price of function 2","developer","developer address"},
+        ...
+        ]
+    }
+    */
+    function getList () public view returns(string memory){
+        string memory result;
+        for(uint index = 0; index < functionNames.length; index++){
+            string memory _name = functionNames[index];
+            string memory nome1 = availableFunctions[_name].name;
+            string memory price1 = uint2str(availableFunctions[_name].price);
+            string memory dev1 = addressToString(availableFunctions[_name].developer);
+
+            if(index != functionNames.length - 1){
+                result = string(abi.encodePacked(result, concat(nome1, price1, dev1), ","));
+            }else{
+                result = string(abi.encodePacked(result, concat(nome1, price1, dev1)));
+            }
+        }
+        result = string(abi.encodePacked("{[",result,"]}"));
+        return result;
+    }
+
+
+    //funzioni "interne" allo smart contract
+    function concat(string memory s1, string memory s2, string memory s3) private pure returns (string memory) {
+        string memory _s1 = string(abi.encodePacked("\"name\"",":","\"",s1,"\""));
+        string memory _s2 = string(abi.encodePacked("\"price\"",":","\"",s2,"\""));
+        string memory _s3 = string(abi.encodePacked("\"developer\"",":","\"",s3,"\""));
+        string memory result = string(abi.encodePacked("{",_s1,",",_s2,",",_s3,"}"));
+        return result;
+    }
+
+    function addressToString(address _addr) public pure returns(string memory) {
+        bytes32 value = bytes32(uint256(_addr));
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(42);
+        str[0] = '0';
+        str[1] = 'x';
+        for (uint i = 0; i < 20; i++) {
+            str[2+i*2] = alphabet[uint(uint8(value[i + 12] >> 4))];
+            str[3+i*2] = alphabet[uint(uint8(value[i + 12] & 0x0f))];
+        }
+        return string(str);
+    }
+
+    function uint2str(uint256 _x) public pure returns (string memory _uintAsString) {
+        uint256 _i = _x;
+        if (_i == 0) {return "0";}
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {len++; j /= 10;}
+        bytes memory bstr = new bytes(len);
+        uint256 k = len - 1;
+        while (_i != 0) {bstr[k--] = bytes1(uint8(48 + (_i % 10))); _i /= 10;}
+        return string(bstr);
+    }
+}
